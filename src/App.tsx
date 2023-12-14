22ff1a8749856be6ffc81ba5ca74c93637018e3e
Riina Hemmilä <riinah97@gmail.com>
2023-12-14 20:10:15 +0200

    added images folder and added wishlist scroll with the text frames

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,55 @@
 import React from 'react';
 import './App.css';
+import scroll from './img/scroll.png';
+
+interface Name {
+  name: string;
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <WishListFrame />
+        <WishListFrame name="user"/>
       </header>
     </div>
   );
 }
 
-function IndividualWishList() {
+function WishList() {
   return (
-    <div>
-      <p>hi!</p>
+    <div className="Wishlist">
+      <ul>
+        <li>
+          <p>load of rubbish</p>
+        </li>
+      </ul>
     </div>
   );
 }
 
-function WishListFrame() {
+function WishListScroll() {
+  return (
+    <div className="ScrollContainer">
+      <div>
+        <img className="WishlistImage" src={scroll} alt="Wishlist scroll" />
+      </div>
+      <div className="ScrollTextContainer">
+        <WishList />
+      </div>
+    </div>
+  );
+}
+
+function WishListFrame(props: Name) {
   return (
     <div>
-      <IndividualWishList />
+      <div>
+        { props.name }
+      </div>
+      <div>
+        <WishListScroll />
+      </div>
     </div>
   );
 }
